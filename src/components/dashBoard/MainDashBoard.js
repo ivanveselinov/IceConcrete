d4fab9c8df4213fcfced9e4b9a058308348e285f
Ivan <ivanveselinov22@gmail.com>
2023-03-13 20:08:55 +1100

    Created 4 more components and setup

--- src/components/dashBoard/MainDashBoard.js
+++ src/components/dashBoard/MainDashBoard.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Home from '../../home/Home'
+import About from '../aboutUs/About'
+import ContactUs from '../contactUs/ContactUs'
+import Projects from '../Projects/Projects'
 
 const MainDashBoard = () => {
   return (
     <div>
       <Home/>
+      <About/>
+      <Projects/>
+      <ContactUs/>
     </div>
   )
 }
