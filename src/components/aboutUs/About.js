c45d3bbd2b6f4037a9be34e37f35ca602e4eb686
Ivan <ivanveselinov22@gmail.com>
2023-03-17 18:59:38 +1100

    About Us read done

--- src/components/aboutUs/About.js
+++ src/components/aboutUs/About.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import AboutUsInput from './AboutUsInput'
+import AboutUsRead from './AboutUsRead'
 
 const About = () => {
 
@@ -13,14 +14,7 @@ const About = () => {
             <p>Image of Ice Concreting</p>
           </div>
           <div className="w-1/3 h-1/2 border m-auto rounded-x p-2 text-xl break-words overflow-scroll">
-            <p>What is Lorem Ipsum?
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including
-            versions of Lorem Ipsum.</p>
+           <AboutUsRead/>
           </div>
         </div>
     </div>
---
+++ src/components/aboutUs/AboutUsRead.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import firebase from 'firebase';
+import { db } from '../../firebase/Firebase';
+
+
+const AboutUsRead = () => {
+
+    const [about, setAbout] = useState();
+
+
+    useEffect(() => {
+        db.collection('about').orderBy('createAt', 'desc').limit(1).onSnapshot(snapshot =>{
+            setAbout(snapshot.docs.map(doc => doc.data()))
+            console.log(`Data: ${snapshot.docs}`);
+        })
+    }, [])
+
+  return (
+    
+    <div className="w-full p-2 border-2 h-40">    
+        {about?.map(({ id, uid, email, about, createAt}) =>
+            <div key={id}>
+               <p className="text-2xl">{about}</p>
+            </div>
+    )}
+    </div>
+  )
+}
+
+export default AboutUsRead
