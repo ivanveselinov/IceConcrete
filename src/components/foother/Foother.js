a4bb43eaa79ee70929e5f4023e8b8f358d9a4ce9
Ivan <ivanveselinov22@gmail.com>
2023-03-22 19:48:03 +1100

    Inserted Hover in Footer

--- src/components/foother/Foother.js
+++ src/components/foother/Foother.js
@@ -6,13 +6,14 @@ import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 const Foother = () => {
 
   const icons = "mb-2";
+  const hoverHeader = "hover:text-blue-300"
 
   return (
     <div className="w-full h-36 inline-flex justify-between bg-gradient-to-r from-orange-600 to-blue-600 rounded-b-xl">
       <div className="w-1/3 block">
         <div className="w-20 h-1/2"></div>
-        <div className="w-80 h-1/2 "> 
-          <a href="https://www.linkedin.com/in/ivanveselinov/" target="blank" >
+        <div className="w-80"> 
+          <a href="https://www.linkedin.com/in/ivanveselinov/" className={hoverHeader} target="blank" >
             <p className="mt-10 ml-2">
               Copyright © 2023 Ivan Veselinov
             </p>
@@ -21,14 +22,13 @@ const Foother = () => {
       </div>
       <div className="w-1/3 h-20 m-auto text-center">
         <p className="text-3xl text-right mt-4"> Follow Us <ThumbUpIcon className={icons} sx ={{ fontSize: 30 }}/></p>
-       
       </div>
       <div className="w-1/3 h-28 m-auto">
         <div className="h-1/2 w-full text-center text-3xl">
-          <a href="https://www.facebook.com/iceconcreting" target="_blank"><FacebookIcon  sx ={{ fontSize: 40 }}/>Facebook</a>
+          <a href="https://www.facebook.com/iceconcreting" className={hoverHeader} target="_blank"><FacebookIcon  sx ={{ fontSize: 40 }}/>Facebook</a>
         </div>
         <div className="h-1/2 w-full text-center text-3xl">
-          <a href="https://www.instagram.com/iceconcreting/" target="_blank" ><InstagramIcon  sx ={{ fontSize: 40 }}/>Instagram</a>
+          <a href="https://www.instagram.com/iceconcreting/" className={hoverHeader} target="_blank" ><InstagramIcon  sx ={{ fontSize: 40 }}/>Instagram</a>
         </div>
       </div>
     </div>
