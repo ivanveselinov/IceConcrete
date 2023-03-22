5dbc183e25e19a29a60e5edf1259dca4218379f5
Ivan <ivanveselinov22@gmail.com>
2023-03-22 18:52:54 +1100

    Email logic added

--- src/components/contactUs/mailer.js
+++ src/components/contactUs/mailer.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { TextareaAutosize } from '@mui/material';
 import Button from '@mui/material/Button';
+import emailjs from 'emailjs-com';
 
 
 const Mailer = () => {
@@ -13,50 +14,78 @@ const Mailer = () => {
 
     const nameChange = (e) => {
         setName(e.target.value);
+        // console.log(setName);
     }
 
     const emailChange = (e) => {
         setEmail(e.target.value)
+        // console.log(setName);
     }
 
     const messageChange = (e) => {
         setMessage(e.target.value)
+        // console.log(setName);
     }
 
     const sendEmail = (e) =>{
+      e.preventDefault();
+      
+      emailjs.sendForm('service_p6ijwr6', 'template_m9phko7', e.target, 'user_dtiDwo5fBoO5jlMqZQ827')
+      .then((result) => {
+          console.log('Your email is send successufuly',result.text);
+      }, (error) => {
+          console.log(error.text);
+      })
         // have to insert a function here!!
+
+    setEmail("")
+    setName("")
+    setMessage("")
     }
 
   return (
    
     <div className="w-3/4 m-auto w-30">
-    <div className="w-1/3 mt-10 m-auto">
-      <div className="bg-red-400 rounded-xl p-1">
-      <input
-              className="w-full p-2 h-12 rounded-xl bg-slate-200"
-              placeholder="Insert Name and Surname?"
-            />
-      </div>
+      <div className="w-1/3 mt-10 m-auto">
+        <form onSubmit={sendEmail}>
+        <div className="bg-red-400 rounded-xl p-1">
+        <input
+                className="w-full p-2 h-12 rounded-xl bg-slate-200"
+                placeholder="Insert Name and Surname?"
+                onChange={nameChange}
+                value={name}
+              />
+        </div>
 
-      <div className="bg-red-400 mt-5 p-1 rounded-xl">
-      <input
-              className="w-full p-2 h-12 rounded-xl bg-slate-200"
+        <div className="bg-red-400 mt-5 p-1 rounded-xl">
+        <input
+                className="w-full p-2 h-12 rounded-xl bg-slate-200"
+                placeholder="Please insert your Email Address"
+                onChange={emailChange}
+                value={email}
+              />
+
+        </div>
+
+        <div className="bg-red-400 mt-5 p-1 rounded-xl mb-5">
+        <textarea
+              className="w-full p-2 h-40 rounded-xl bg-slate-200"
               placeholder="Please insert your Email Address"
-            />
+              onChange={messageChange}
+              value={message}
+        />
+        </div>
+        
+        <Button 
+            variant="contained" 
+            color="success"
+            type="submit"
+            value="send"
+            >      
+          Send
+        </Button>
+        </form>
       </div>
-
-      <div className="bg-red-400 mt-5 p-1 rounded-xl mb-5">
-      <textarea
-            className="w-full p-2 h-40 rounded-xl bg-slate-200"
-            placeholder="Please insert your Email Address"
-      />
-      </div>
-      
-      <Button variant="contained" color="success">      
-        Send
-      </Button>
-
-    </div>
    </div>
           
 
