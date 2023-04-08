9fde96e5a5b733af73d7790e0dacc215c69df534
Ivan <ivanveselinov22@gmail.com>
2023-04-08 14:45:26 +1000

    Test for deployment issue

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyC6YAQQaagvQgrWCWlNVSqwppIq13BpJbg",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
