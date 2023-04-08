d8e85ddc9813ccbdd88bb4b9297fef6d702c63a2
Ivan <ivanveselinov22@gmail.com>
2023-04-08 12:49:59 +1000

    Fixed Project to be responsive

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
   return (
   <Fade duration={5000}>
     <div className="w-full pt-24" id="projects">
-          <h1 className="text-center p-10 text-4xl">Projects </h1>
+          <h1 className="text-center p-10 text-4xl xs:text-2xl md:text-2xl lg:text-2xl xl:text-4xl">Projects </h1>
           
        {appUser.uid && 
         <div className="w-1/3 p-1 space-x-2 m-auto rounded-xl mb-2 shadow-sm bg-slate-400">
--- src/components/Projects/ProjectsRead.js
+++ src/components/Projects/ProjectsRead.js
@@ -54,11 +54,11 @@ const ProjectsRead = () => {
               {projects?.map(({id, uid, postImage, projects, createAt}) =>
                 {
                     
-                      return <div className="w-[45%] ml-10 inline-block mb-2 bg-slate-400 rounded-xl" key={id}>
+                      return <div className=" ml-10 inline-block mb-2 bg-slate-400 rounded-xl xs:w-4/5 md:w-4/5 lg:w-[45%]" key={id}>
                         <div className="bg-red-500 rounded-xl">
                           <div className="w-full flex rounded-xl">
-                              <div className="w-1/2 flex mt-1 mb-1 ml-1 ">
-                                  <CardMedia className="rounded-l-xl bg-slate-400"
+                              <div className="w-1/2 flex mt-1 mb-1 ml-1 xs:w-[98%]">
+                                  <CardMedia className="rounded-l-xl bg-slate-400 xs:rounded-r-xl md:rounded-r-none lg:rounded-r-none"
                                       style={{
                                           display: "flex",
                                           alignItems: "center",
@@ -71,7 +71,7 @@ const ProjectsRead = () => {
                                       image={postImage}
                                       alt={postImage} />
                               </div>
-                              <div className="min-w-1/2 w-full flex bg-slate-400 mt-1 mb-1 mr-1 rounded-r-xl">
+                              <div className="min-w-1/2 w-full flex bg-slate-400 mt-1 mb-1 mr-1 rounded-r-xl xs:hidden sm:hidden md:block">
                                   <div key={id} className="w-full p-2 break-words overflow-auto mb-2 mr-1 h-full"
                                       style={{
                                           justifyContent: "center",
